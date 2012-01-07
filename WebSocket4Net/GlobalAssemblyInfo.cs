5ca58f7b2e375330124584d026b1b226b70e8901
zjiang@w-shdc-zjiang.global.tektronix.net <zjiang@w-shdc-zjiang.global.tektronix.net>
2012-01-07 21:45:26 +0800

    updated assembly version

--- WebSocket4Net/GlobalAssemblyInfo.cs
+++ WebSocket4Net/GlobalAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © websocket4net.codeplex.com 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
