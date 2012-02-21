67622a41592f0fcd3bae8e7512b94e8c9d0d915a
zjiang@w-shdc-zjiang.global.tektronix.net <zjiang@w-shdc-zjiang.global.tektronix.net>
2012-02-21 11:50:20 +0800

    added missed file

---
+++ WebSocket4Net/Properties/AssemblyInfo.MonoDroid.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebSocket4Net for Mono for Android")]
+[assembly: AssemblyDescription("WebSocket4Net for Mono for Android")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8767F242-8DD1-4BF0-8A10-D10FF0492983")]
