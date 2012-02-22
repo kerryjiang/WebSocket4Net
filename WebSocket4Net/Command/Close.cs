b195e675a16dc203d557589b27623ffbae9bbbdd
zjiang@w-shdc-zjiang.global.tektronix.net <zjiang@w-shdc-zjiang.global.tektronix.net>
2012-02-22 12:58:06 +0800

    fixed a bug firing error event incorrect

--- WebSocket4Net/Command/Close.cs
+++ WebSocket4Net/Command/Close.cs
@@ -13,8 +13,12 @@ namespace WebSocket4Net.Command
             //Close handshake was sent from client side, now got a handshake response
             if (session.State == WebSocketState.Closing)
             {
-                if (commandInfo.CloseStatusCode > 0 || !string.IsNullOrEmpty(commandInfo.Text))
+                //Not NormalClosure
+                if (commandInfo.CloseStatusCode != session.ProtocolProcessor.CloseStatusCode.NormalClosure &&
+                    (commandInfo.CloseStatusCode > 0 || !string.IsNullOrEmpty(commandInfo.Text)))
+                {
                     session.FireError(new Exception(string.Format("{0}: {1}", commandInfo.CloseStatusCode, commandInfo.Text)));
+                }
                 session.CloseWithouHandshake();
                 return;
             }
