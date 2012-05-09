3c90f859d9370e62c4083176ceec7c7a8011994d
Kerry Jiang <kerry-jiang@hotmail.com>
2012-05-09 21:31:16 +0800

    added more parameters for constructor of JsBridge

---
+++ JsBridge/WebSocket4Net.JsBridge/ClientAccessPolicyProtocol.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Windows.Browser;
+using System.ComponentModel;
+
+namespace WebSocket4Net.JsBridge
+{
+    public enum ClientAccessPolicyProtocol
+    {
+        Http = 0,
+        Tcp = 1
+    }
+}
