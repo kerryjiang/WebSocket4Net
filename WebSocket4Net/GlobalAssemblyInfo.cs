e5a2de44108791aa0ceb53f9f628f745f970e8af
Kerry Jiang <kerry-jiang@hotmail.com>
2012-06-11 19:46:42 +0800

    updated assembly version

--- WebSocket4Net/GlobalAssemblyInfo.cs
+++ WebSocket4Net/GlobalAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © websocket4net.codeplex.com 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
