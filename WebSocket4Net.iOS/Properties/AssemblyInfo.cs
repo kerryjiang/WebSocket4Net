0f8b8a1c153ed1643964e86aeafea241728d62f4
Kerry Jiang <kerry-jiang@hotmail.com>
2015-03-23 10:37:14 +0800

    Added iOS Unified API support

---
+++ WebSocket4Net.iOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebSocket4Net for iOS Unified API")]
+[assembly: AssemblyDescription("WebSocket4Net for iOS Unified API")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b59453fe-1b25-463d-a7dd-10f1cf5dbe06")]
