5d8213cb76af3d3f274542cfad71ae7c770dc488
Kerry Jiang <kerry-jiang@hotmail.com>
2015-03-23 11:23:46 +0800

    Added assembly info for MonoTouch project

--- WebSocket4Net.MonoTouch/Properties/AssemblyInfo.cs
+++ WebSocket4Net.MonoTouch/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("WebSocket4Net for Silverlight")]
-[assembly: AssemblyDescription("WebSocket4Net for Silverlight")]
+[assembly: AssemblyTitle("WebSocket4Net for iOS Unified API")]
+[assembly: AssemblyDescription("WebSocket4Net for iOS Unified API")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b59453fe-1b25-463d-a7dd-10f1cf5dbe06")]
