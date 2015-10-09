c54a0cf13a3469d3237d4878f3e62c88695194f7
Kerry Jiang <kerry-jiang@hotmail.com>
2015-10-09 23:09:09 +0800

    updated the version number to 0.14.1

--- WebSocket4Net/GlobalAssemblyInfo.cs
+++ WebSocket4Net/GlobalAssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: AssemblyCopyright("Copyright © websocket4net.codeplex.com 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.14.0.0")]
-[assembly: AssemblyFileVersion("0.14.0.0")]
+[assembly: AssemblyVersion("0.14.1.0")]
+[assembly: AssemblyFileVersion("0.14.1.0")]
