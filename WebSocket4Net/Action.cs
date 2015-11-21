ed336df2ba37687e48adfcc4ea533d5ff324e0b1
Kerry Jiang <kerry-jiang@hotmail.com>
2015-11-21 15:58:41 +0800

    upgraded to SuperSocket.ClientEngine 0.6.0.3

---
+++ WebSocket4Net/Action.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public delegate void Action<T1, T2>(T1 t1, T2 t2);
+
+    public delegate void Action<T1, T2, T3>(T1 t1, T2 t2, T3 t3);
+
+    public delegate void Action<T1, T2, T3, T4>(T1 t1, T2 t2, T3 t3, T4 t4);
+}
