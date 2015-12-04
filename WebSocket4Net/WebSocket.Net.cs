ccc87d39b370b6763a4e5e9babd7b6aba7f6ff18
Kerry Jiang <kerry-jiang@hotmail.com>
2015-12-04 22:44:43 +0800

    upgraded SuperSocket.ClientEngine

--- WebSocket4Net/WebSocket.Net.cs
+++ WebSocket4Net/WebSocket.Net.cs
@@ -46,9 +46,13 @@ namespace WebSocket4Net
 
         }
 
-        public WebSocket(string uri, string subProtocol = "", List<KeyValuePair<string, string>> cookies = null, List<KeyValuePair<string, string>> customHeaderItems = null, string userAgent = "", string origin = "", WebSocketVersion version = WebSocketVersion.None, EndPoint httpConnectProxy = null, SslProtocols sslProtocols = SslProtocols.Default)
+        public WebSocket(string uri, string subProtocol = "", List<KeyValuePair<string, string>> cookies = null, List<KeyValuePair<string, string>> customHeaderItems = null, string userAgent = "", string origin = "", WebSocketVersion version = WebSocketVersion.None, EndPoint httpConnectProxy = null, SslProtocols sslProtocols = SslProtocols.None)
         {
-            m_SecureProtocols = sslProtocols;
+#if !NETFX_CORE
+            if (sslProtocols != SslProtocols.None)
+                m_SecureProtocols = sslProtocols;
+#endif
+
             Initialize(uri, subProtocol, cookies, customHeaderItems, userAgent, origin, version, httpConnectProxy);
         }
     }
