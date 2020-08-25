befd2919c43734386316f41274070bc6067bcbc0
Kerry Jiang <kerry-jiang@hotmail.com>
2020-08-25 16:55:12 -0700

    added a new test case called TestEchoMessage

--- test/WebSocket4Net.Tests/MainTest.cs
+++ test/WebSocket4Net.Tests/MainTest.cs
@@ -18,7 +18,6 @@ namespace WebSocket4Net.Tests
         }
 
         [Theory]
-        [Trait("Category", "TestHandshake")]
         [InlineData(typeof(RegularHostConfigurator))]
         [InlineData(typeof(SecureHostConfigurator))]
         public async ValueTask TestHandshake(Type hostConfiguratorType) 
@@ -63,6 +62,76 @@ namespace WebSocket4Net.Tests
                 // test path
                 Assert.Equal(path, serverSessionPath);
                 Assert.True(connected);
+
+                await websocket.CloseAsync();
+
+                Assert.NotNull(websocket.CloseStatus);
+                Assert.Equal(CloseReason.NormalClosure, websocket.CloseStatus.Reason);
+
+                await Task.Delay(1 * 1000);
+
+                //Assert.Equal(WebSocketState.Closed, websocket.State);
+                Assert.False(connected);
+
+                await server.StopAsync();
+            }
+        }
+
+
+        [Theory]
+        [InlineData(typeof(RegularHostConfigurator), 10)]
+        [InlineData(typeof(SecureHostConfigurator), 10)]
+        public async ValueTask TestEchoMessage(Type hostConfiguratorType, int repeat) 
+        {
+            var hostConfigurator = CreateObject<IHostConfigurator>(hostConfiguratorType);
+
+            var serverSessionPath = string.Empty;
+            var connected = false;
+
+            using (var server = CreateWebSocketSocketServerBuilder(builder =>
+            {
+                builder.UseSessionHandler(async (s) =>
+                {
+                    connected = true;
+                    await Task.CompletedTask;
+                },
+                async (s) =>
+                {
+                    connected = false;
+                    await Task.CompletedTask;
+                });
+
+                builder.UsePackageHandler(async (s, p) =>
+                {
+                    var session = s as WebSocketSession;
+                    await session.SendAsync(p.Message);
+                });
+
+                return builder;
+            }, hostConfigurator: hostConfigurator)
+                .BuildAsServer())
+            {
+                Assert.True(await server.StartAsync());
+                OutputHelper.WriteLine("Server started.");
+
+                var websocket = new WebSocket($"{hostConfigurator.WebSocketSchema}://localhost:{hostConfigurator.Listener.Port}");
+
+                hostConfigurator.ConfigureClient(websocket);
+
+                await websocket.OpenAsync();
+
+                //Assert.Equal(WebSocketState.Open, websocket.State);
+
+                await Task.Delay(1 * 1000);                
+                Assert.True(connected);
+
+                for (var i = 0; i < repeat; i++)
+                {
+                    var text = Guid.NewGuid().ToString();
+                    await websocket.SendAsync(text);
+                    var receivedText = (await websocket.ReceiveAsync()).Message;
+                    Assert.Equal(text, receivedText);
+                }
 
                 await websocket.CloseAsync();
 
